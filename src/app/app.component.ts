8b7ff62d200cec2fc5cadc2f980de1dac01d4e4f
oomusou <oomusou@gmail.com>
2017-05-22 14:45:17 +0800

    傳統建立物件方式

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Hero } from './models/hero';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  heroes: Hero[] = [];
+
+  onAddHeroClick() {
+    const hero = new Hero();
+    hero.name = 'Sam';
+    hero.state = 'active';
+    this.heroes.push(hero);
+  }
 }
